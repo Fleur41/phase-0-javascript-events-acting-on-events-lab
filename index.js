5ff246fb8994edcb271742f2eaade28974a2bbed
Fleur41 <Muriithikings456@gmail.com>
2022-11-22 16:17:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+
+  
+  function moveDodgerRight() {
+    
+    const fake = document.getElementById("fake")
+    const game = document.getElementById("game");
+    const game_max_width = game.getBoundingClientRect();
+    const int_game_max_width = parseInt(game_max_width, 10); 
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+
+    if ( left !== 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
